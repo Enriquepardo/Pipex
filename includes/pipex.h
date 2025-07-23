873b110c2997dd05e71fa7e972c6ff62b57d381b
Enriquepardo <quiquepardos@gmail.com>
2025-07-24 00:03:46 +0200

    WIP

---
+++ includes/pipex.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: enpardo- <enpardo-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/23 23:57:42 by enpardo-          #+#    #+#             */
+/*   Updated: 2025/07/23 23:58:02 by enpardo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include "../libft/libft.h"
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/wait.h>
+# include <unistd.h>
+
+// Mandatory functions
+void	error(void);
+char	*find_path(char *cmd, char **envp);
+int		get_next_line(char **line);
+void	execute(char *argv, char **envp);
+
+// Bonus functions
+int		open_file(char *argv, int i);
+void	usage(void);
+
+#endif
